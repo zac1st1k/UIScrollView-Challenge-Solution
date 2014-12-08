719e20bbdf53c4f55c0e3a773b2c6e2a394097a7
zac1st1k <zacfirst@gmail.com>
2014-12-08 12:27:57 +1100

    Initial Commit

---
+++ UIScrollView Challenge Solution/XZZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XZZAppDelegate.h
+//  UIScrollView Challenge Solution
+//
+//  Created by Zac on 8/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
